f944a453d8635ebcbb480cfbf824f5096eec393c
Mahmud1087 <abdulazeezm578@gmail.com>
2025-11-09 15:44:18 +0100

    feat(home): add home screen first page

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,59 @@
-import { Text, View } from 'react-native';
+import onboarding from '@/assets/images/onboarding.png';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='text-xl font-josefinSans text-success text-center px-6'>
-        Welcome to Famez, your number one social media app
-      </Text>
+    <View className='flex-1 items-center justify-center bg-gray-950 text-white-100'>
+      <View className='app-container'>
+        <View className='w-full'>
+          <Image
+            source={onboarding}
+            alt='Onboarding Image'
+            resizeMode='contain'
+            className='w-full'
+          />
+        </View>
+        <Text className='text-white-100 font-josefinSans-bold text-4xl mb-3 text-center'>
+          Welcome to Framez
+        </Text>
+        <Text className='text-gray-400 text-center font-josefinSans-medium text-xl'>
+          Where you get to connect with your friends, post, share, and connect
+        </Text>
+
+        <TouchableOpacity className='mt-10 w-full bg-gradient-to-r'>
+          <LinearGradient
+            colors={['#4c669f', '#3b5998', '#192f6a']}
+            style={styles.gradient}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}
+          >
+            <Text className='text-white-100 font-josefinSans-medium text-2xl'>
+              Get Started
+            </Text>
+          </LinearGradient>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  gradient: {
+    width: '100%',
+    height: 50,
+    borderRadius: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
